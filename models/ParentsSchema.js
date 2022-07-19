9f001d35f781ae205a8b5d13aeeaae5ba1536f26
Krystal <krystalkoh@gmail.com>
2022-07-19 17:04:13 +0800

    update

--- models/ParentsSchema.js
+++ models/ParentsSchema.js
@@ -17,7 +17,8 @@ const ParentsSchema = new mongoose.Schema(
     },
     phone: { type: String, required: true },
     address: { type: String, required: true },
-    assignments: [{
+    assignments: [
+      {
         childName: {
           type: String,
           required: true,
@@ -44,13 +45,15 @@ const ParentsSchema = new mongoose.Schema(
           required: true,
           default: "select",
         },
-        subject: [{ type: String, default: "" }],
-        duration: {type: String, default: ""}, 
-        frequency: {type: String, default: ""},
-        days: {type: String, default: "" },
+        subject: { type: String, default: "" },
+        duration: { type: String, default: "" },
+        frequency: { type: String, default: "" },
+        days: { type: String, default: "" },
         rate: { type: String, default: "" },
         availability: { type: Boolean, default: true },
-      }],
+        tutorsApplied: [],
+      },
+    ],
     role: { type: String, default: "Parent" },
   },
   { collection: "parents" }
